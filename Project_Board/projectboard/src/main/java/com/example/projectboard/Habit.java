1ffd5e52bf665e2f8e30f33207282193e8f7c5f2
unknown <kamkucharsky@gmail.com>
2019-04-23 16:57:29 +0200

    Add ORM

--- Project_Board/projectboard/src/main/java/com/example/projectboard/Habit.java
+++ Project_Board/projectboard/src/main/java/com/example/projectboard/Habit.java
@@ -1,7 +1,13 @@
 package com.example.projectboard;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Habit {
 
+    @Id
     private String id;
     private String name;
     private String days;
---
+++ Project_Board/projectboard/src/main/java/com/example/projectboard/HabitRepository.java
@@ -0,0 +1,10 @@
+package com.example.projectboard;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface HabitRepository extends CrudRepository<Habit, String> {
+
+
+
+
+}
